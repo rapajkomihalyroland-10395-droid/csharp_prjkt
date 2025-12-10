4dd7088bd6da1a20bf17db9ce94b16475da7402e
rapajkomihalyroland-10395-droid <rapajkomihalyroland-10395@taszi.hu>
2025-12-10 18:25:28 +0100

    Add Book model

---
+++ csharpPrjkt/csharpPrjkt/Models/Book.cs
@@ -0,0 +1,16 @@
+using SQLite;
+
+namespace csharpPrjkt.Models
+{
+    [Table("Books")]
+    public class Book
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PublishedYear { get; set; }
+    }
+}
